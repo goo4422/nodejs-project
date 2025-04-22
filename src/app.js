416bb5099161b945a63968c757383e059115e542
Ubuntu <ubuntu@ip-172-31-38-80.ap-southeast-1.compute.internal>
2025-04-22 07:03:00 +0000

    change deployment

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index', {
         title: 'Weather App for Docker course',
-        name: 'Shinebayar. Ts 2025.04.22 14.55'
+        name: 'Shinebayar. Ts 2025.04.22 15.02'
     })
 })
 app.get('/about',(req,res)=>{
