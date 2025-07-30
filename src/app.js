cccdb3a75f3a5bf86df8e19afff9c183955d4d7c
asd123 Goo22 <asd123goo22@asd123s-MacBook-Pro.local>
2025-07-30 15:00:33 +0800

    q

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get('',(req,res)=>{
 })
 app.get('/about',(req,res)=>{
     res.render('about', {
-        title: 'About Me',
+        title: 'About purevdorj',
         name: 'purevdorj'
     })
 })
