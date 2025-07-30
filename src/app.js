f631bc9a37f6bd39976e22a79e2cfec8dad3ccde
asd123 Goo22 <asd123goo22@asd123s-MacBook-Pro.local>
2025-07-30 14:39:38 +0800

    change

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index', {
         title: 'Weather App for Docker course',
-        name: 'purevdorj'
+        name: 'purevdorj 14:40'
     })
 })
 app.get('/about',(req,res)=>{
